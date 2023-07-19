491af49c05dfff0347b6c9b336917586f307df6d
ChoiSW99 <84121891+ChoiSW99@users.noreply.github.com>
2023-07-19 18:37:08 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 104 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -3,41 +3,35 @@ from collections import deque
 input=sys.stdin.readline
 
 
-
 while(True):
-    str = input().rstrip()
     stack = []
-    small = 0
-    big = 0
-    
-    # 종료 조건
-    if(str == '.'):
+    str = input().rstrip()
+    if str == '.':
         break
-
-    for c in str:
-        if(c == '(' or c == ')' or c == '[' or c == ']'):
-            stack.append(c)
     
-    
-    isDeleted = True
-    while(isDeleted):
-        isDeleted = False
-
-        for i in range(len(stack)-1):
-            if stack[i] =='(' and stack[i+1] == ')':
-                stack.pop(i+1)
-                stack.pop(i)
-                isDeleted = True
+    for x in str:
+           
+        if x == '(' or x == '[':
+            stack.append(x)
+            
+        elif x == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(')')
                 break
-            elif stack[i] =='[' and stack[i+1] == ']':
-                stack.pop(i+1)
-                stack.pop(i)
-                isDeleted = True
+            
+        elif x == ']':
+            if stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(']')
                 break
-
-    if stack:
-        print("no")
+            
+    if(len(stack) == 0):
+        print('yes')
     else:
-        print("yes")   
-        
-#([(([([])()(())]))])
+        print('no')
+
+
+    
