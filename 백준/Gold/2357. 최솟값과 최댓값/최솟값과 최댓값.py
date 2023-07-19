362c532a8362abad80500355b276d60d17490493
ChoiSW99 <84121891+ChoiSW99@users.noreply.github.com>
2023-07-19 13:25:36 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 1824 ms, Memory: 70068 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
@@ -12,45 +12,45 @@ for _ in range(N):
     arr.append(int(input()))
 #print(arr)
 
-def Build(_segTree, node, start, end):
+def Build(node, start, end):
     # leaf node
     
     if(start == end):
-        _segTree[node] = [arr[start], arr[start]]
+        segTree[node] = [arr[start], arr[start]]
         #print(segTree)
-        return _segTree[node]
+        return segTree[node]
     else:
         
         mid = (start + end)//2
-        left = Build(_segTree, node*2, start, mid)
-        right = Build(_segTree, node*2+1, mid+1, end)
-        _segTree[node][0] = min(left[0],right[0])
-        _segTree[node][1] = max(left[1],right[1])
+        left = Build(node*2, start, mid)
+        right = Build(node*2+1, mid+1, end)
+        segTree[node][0] = min(left[0],right[0])
+        segTree[node][1] = max(left[1],right[1])
         #print(segTree)
-        return _segTree[node]
+        return segTree[node]
     
 #print(segTree)
-Build(segTree, 1, 0, N-1)
+Build(1, 0, N-1)
 
 #print(segTree)
 
 minList = []
 maxList = []
 
-def Find(_segTree, node, subStart, subEnd, start , end):
+def Find(node, subStart, subEnd, start , end):
     if(subStart <= start and end <= subEnd):
-        minList.append(_segTree[node][0])
-        maxList.append(_segTree[node][1])
+        minList.append(segTree[node][0])
+        maxList.append(segTree[node][1])
         return
     if(subStart > end or subEnd < start):
         return
     mid = (start + end)//2
-    Find(_segTree, node*2, subStart, subEnd, start, mid)
-    Find(_segTree, node*2+1, subStart, subEnd, mid+1, end)
+    Find(node*2, subStart, subEnd, start, mid)
+    Find(node*2+1, subStart, subEnd, mid+1, end)
 
 for _ in range(M):
     a, b = map(int, input().split())
-    Find(segTree, 1, a-1, b-1, 0, N-1)
+    Find(1, a-1, b-1, 0, N-1)
     #print("minList ", minList)
     #print("maxList ", maxList)
     print(min(minList), max(maxList))
