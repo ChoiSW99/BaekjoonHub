e0dc3e9844cd2887a27c7465241fcdeb498bd2de
ChoiSW99 <84121891+ChoiSW99@users.noreply.github.com>
2023-07-25 17:09:50 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 56 ms, Memory: 40604 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,54 +1,34 @@
 import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(1000000) # 재귀 깊이 제한 늘리기
+sys.setrecursionlimit(10**6)
 
-N = int(input()) # 3의 거듭제곱
-arr = [['*']*N for _ in range(N)]
+def append_star(LEN):
+    if LEN == 1:
+        return ['*']
 
-def Print():
-    for i in range(N):
-        for j in range(N):
-            print(arr[i][j], end = '')
-        print()
-    
-def Pattern(N, xx, yy):
-    for i in range((N//3), (N//3)*2):
-        for j in range((N//3), (N//3)*2):
-            arr[xx+i][yy+j] = ' '
+    Stars = append_star(LEN//3) 
+    L = []  
+    #print('STARS', Stars)
+    for S in Stars:
+        L.append(S + S + S)
+    for S in Stars:
+        L.append(S + ' '*(LEN//3) + S)
+    for S in Stars:
+        L.append(S + S + S)
+    #print(L)
+    return L
 
-    if N != 3:                
-        for x in range(3):
-            for y in range(3):
-                Pattern(N//3, xx+(N//3)*x, yy+(N//3)*y)
-                
-                
+n = int(sys.stdin.readline().strip())
+print('\n'.join(append_star(n)))
 
-
-Pattern(N, 0, 0)
-Print()
 '''
-27 0 0
-    9 0 0
-        (n=3)
-        3 0 0
-        3 0 3
-        3 0 6
-        3 3 0
-        3 3 3 X
-        3 3 6
-        3 6 0
-        3 6 3
-        3 6 6
-    
-    9 0 9
-        3 0 9
-        3 0 12
-        3 0 15
-    
-    9 0 18
-    9 9 0
-    9 9 18
-    9 2 0
-    9 2 1
-    9 2 2 
+Stars=ap_star(27)
+        Stars=ap_star(9)
+        ('***', '* *', '***')
+                Stars=ap_star(3)
+                        [*]=Stars=ap_star(1)
+                L.append(***)
+                L.append(* *)
+                L.append(***)    
+
+
 '''
