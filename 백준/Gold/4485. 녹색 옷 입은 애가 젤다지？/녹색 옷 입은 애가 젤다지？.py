3893cf41b67df82470dabf1ce22f9ab72d839bf4
ChoiSW99 <84121891+ChoiSW99@users.noreply.github.com>
2023-08-03 16:12:51 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 128 ms, Memory: 33324 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -0,0 +1,34 @@
+import sys, heapq
+input = sys.stdin.readline
+
+Tc = 1
+dx = [-1, 1,  0, 0]
+dy = [ 0, 0, -1, 1]
+
+def Dijkstra():
+    lose = [list(map(int, input().split())) for _ in range(N)]
+    loseSum = [[float('inf') for _ in range(N)] for _ in range(N)]
+    loseSum[0][0] = lose[0][0]
+    
+    que = [(lose[0][0], 0, 0)] # lose, i, j
+    while que:
+        lsum, i, j = heapq.heappop(que)
+        
+        for idx in range(4):
+            if 0 <= i+dx[idx] <= N-1 and 0<= j+dy[idx] <= N-1:
+                newLose = lsum + lose[i+dx[idx]][j+dy[idx]] 
+                if newLose < loseSum[i+dx[idx]][j+dy[idx]]:
+                    loseSum[i+dx[idx]][j+dy[idx]] = newLose
+                    heapq.heappush(que, (newLose, i+dx[idx], j+dy[idx]))
+                    
+    return loseSum[N-1][N-1]
+
+while True:
+    N = int(input())
+    if N == 0:
+        break
+    
+    result = Dijkstra()
+    
+    print('Problem ' + str(Tc) + ': ' + str(result))
+    Tc += 1
