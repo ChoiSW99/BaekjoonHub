e163530044c36252cd05077ee0ba4e3b6a29b1e5
ChoiSW99 <84121891+ChoiSW99@users.noreply.github.com>
2023-08-08 15:10:54 +0900

    [Gold V] Title: 평범한 배낭, Time: 4432 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -6,21 +6,22 @@ N, K = map(int, input().split())
 
 w = [0]
 v = [0]
+dp = [0] * (K+1) # dp[i] : 가방용량이 i일 때의 최대가치합
 for _ in range(N):
     # 무게, 가격
     W, V = map(int,input().split())
     w.append(W)
     v.append(V)
 
-# dp[i][j] : 1~i번 보석을 고려하고 가방 용량이 j일 때, 가치합의 최대값
+# dp[i][j] : 1~i번 보석을 고려하고 가방 용량이 j일 때, 최대가치합
 # dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i])
-dp = [[0] * (K+1) for _ in range(N+1)]
 
 for i in range(1, N+1):
-    for j in range(1, K+1):
+    for j in range(K, 0, -1): # 가방용량 j
         if j-w[i] >= 0: # i번째 보석을 넣을 수 있는 경우
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]) # 넣기, 안넣기 비교
-        else: # i번째 보석을 넣을 수 없는 경우
-            dp[i][j] = dp[i-1][j] # 가방용량은 유지한채 i-1번째 보석까지 고려한 것과 같음
+            dp[j] = max(dp[j], dp[j-w[i]]+v[i]) # 안넣을때 vs 넣을때 
+        #else: # i번째 보석을 넣을 수 있는 경우
+            # 그대로 유지
 
-print(dp[N][K])
+
+print(dp[K])
