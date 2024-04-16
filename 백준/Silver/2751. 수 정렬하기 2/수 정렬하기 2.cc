d37f45373b738bdb03e484e7d6028363c45faeb7
SeungWoo <84121891+ChoiSW99@users.noreply.github.com>
2024-04-16 17:28:47 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 332 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[1000001];
+int temp[1000001];
+
+void Merge(int st, int mid, int end)
+{
+	int l_idx = st;		// [st, mid) 
+	int r_idx = mid;	//			[mid, end)
+	for (int i = st; i < end; i++)
+	{
+		if (r_idx == end) // 오른쪽 배열을 다 넣음
+			temp[i] = arr[l_idx++];
+		else if (l_idx == mid) // 왼쪽 배열을 다 넣음
+			temp[i] = arr[r_idx++];
+		else if (arr[l_idx] <= arr[r_idx])
+			temp[i] = arr[l_idx++];
+		else
+			temp[i] = arr[r_idx++];
+	}
+
+	for (int i = st; i < end; i++)
+		arr[i] = temp[i];
+}
+
+void MergeSort(int st, int end) // [st, end) 배열
+{
+	if (st == end - 1) return; // 1칸짜리
+	int mid = (st + end) / 2;
+	MergeSort(st, mid);
+	MergeSort(mid, end);
+
+	Merge(st, mid, end);
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+	MergeSort(0, N);
+	for (int i = 0; i < N; i++)
+		cout <<  arr[i] << '\n';
+}
