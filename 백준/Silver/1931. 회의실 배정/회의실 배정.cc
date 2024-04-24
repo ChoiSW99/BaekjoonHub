3df47bddc964a9e954e4698391a45f523a3bb2a5
SeungWoo <84121891+ChoiSW99@users.noreply.github.com>
2024-04-24 18:33:59 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,57 @@
+/*
+https://www.acmicpc.net/problem/1931
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using pll = pair<long long, long long>;
+
+bool comp(pll& p1, pll& p2)
+{
+	if (p1.second != p2.second) 
+		return (p1.second < p2.second);
+	return p1.first < p2.first;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int N;
+	cin >> N;
+	vector<pll> inputs;
+	int s, e;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> s >> e;
+		inputs.emplace_back(s,e);
+	}
+
+	sort(inputs.begin(), inputs.end(), comp);
+
+	int answer = 0;
+	pll cur = { -1,-1 };
+
+	for (int i = 0; i < N; i++) 
+	{
+		pll next = inputs[i];
+		if (next.first < cur.second)
+			continue;
+
+		cur = next;
+		answer++;
+	}
+	cout << answer;
+
+	return 0;
+}
+
+/*
+ ----
+   ---
+-------
+     ---
+
+*/
