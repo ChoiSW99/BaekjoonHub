dee110e266f878c1d22422cb327ae9903d3d0d61
SeungWoo <84121891+ChoiSW99@users.noreply.github.com>
2024-04-30 19:58:57 +0900

    [Silver I] Title: 스티커, Time: 84 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -30,10 +30,7 @@ int main()
 		dp[0][0] = sticker[0][0];
 		dp[1][0] = sticker[1][0];
 
-		dp[0][1] = max(dp[0][0], dp[1][0] + sticker[0][1]);
-		dp[1][1] = max(dp[1][0], dp[0][0] + sticker[1][1]);
-
-		for (int i = 2; i < n; i++)
+		for (int i = 1; i < n; i++)
 		{
 			dp[0][i] = max( dp[0][i - 1], (dp[1][i - 1] + sticker[0][i]) );
 			dp[1][i] = max( dp[1][i - 1], (dp[0][i - 1] + sticker[1][i]) );
