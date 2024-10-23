27f21e92eadb42aec70017d3b909abbd9a2fc6a8
SeungWoo <84121891+ChoiSW99@users.noreply.github.com>
2024-10-23 15:38:22 +0900

    [level 2] Title: 타겟 넘버, Time: 20.78 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,24 +1,34 @@
-#include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-void DFS(vector<int>& numbers, int target, int current, int depth, int& answer)
-{
-    if (depth == numbers.size())
-    {
-        if (current == target)
-            answer++;
-        return;
-    }
-    
-    DFS(numbers, target, current + numbers[depth], depth + 1, answer);
-    DFS(numbers, target, current - numbers[depth], depth + 1, answer);
-}
-
 int solution(vector<int> numbers, int target) {
     int answer = 0;
-    DFS(numbers, target, 0, 0, answer);
+    queue<pair<int, int>> que;
+    
+    que.push({numbers[0], 0});
+    que.push({(-1) * numbers[0], 0});
+    
+    while(!que.empty())
+    {
+        auto now = que.front();
+        que.pop();
+        int curSum = now.first;
+        int curIdx = now.second;
+        
+        // 확인하기
+        if(curIdx == numbers.size()-1)
+        {
+            if(curSum == target)
+                answer++;
+            continue;
+        }
+        
+        que.push({curSum+numbers[curIdx+1], curIdx+1});
+        que.push({curSum-numbers[curIdx+1], curIdx+1});
+    }
+    
     return answer;
 }
