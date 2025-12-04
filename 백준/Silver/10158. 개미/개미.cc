97f558104fd655c5daf0424827cf166e6eb4d16f
SeungWoo <84121891+ChoiSW99@users.noreply.github.com>
2025-12-04 14:35:50 +0900

    [Silver III] Title: 개미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.cc
+++ 백준/Silver/10158. 개미/개미.cc
@@ -6,41 +6,6 @@ int w, h; // 격자 w, h
 int p, q; // 초기위치 (p, q)
 int t;    // 시간t
 
-int Calc(int nowPos, int leftMove, int maxPos)
-{
-    int newPos = nowPos;
-    
-    int distToMax = maxPos-newPos;
-    if (leftMove > distToMax)
-    {
-        // max까지 증가
-        leftMove -= distToMax;
-        newPos = maxPos;
-        
-        if (leftMove > newPos)
-        {
-            // 0까지 이동
-            leftMove -= newPos;
-            newPos = 0;
-            
-            
-            // max쪽으로 이동
-            newPos += leftMove;
-            leftMove = 0;
-        }
-        else
-        {
-            newPos -= leftMove;
-        }
-    }
-    else
-    {
-        newPos += leftMove;
-    }
-    
-    return newPos;
-}
-// 방향전환 없이 쭉 간다고 치면 p+t에 위치. 이 때 2*w로 나눈 나머지만큼 
 
 int main()
 {
@@ -53,11 +18,13 @@ int main()
     
     // x좌표(좌우) y좌표(상하) 따로 계산하기
     
-    int moveX = t % (2 * w);    // 2*w만큼 이동하면 제자리임
-    int newX = Calc(p, moveX, w);
-    
-    int moveY = t % (2 * h);
-    int newY = Calc(q, moveY, h);
+    int newX = (p+t) % (2*w);
+    if (newX > w) // 꺾임 (w를 넘어간 부분을 w 아래로 접어줌)  
+        newX = (2*w) - newX;
+        
+    int newY = (q+t) % (2*h);
+    if (newY > h)
+        newY = (2*h) - newY;
     
     cout << newX << " " << newY;
     
